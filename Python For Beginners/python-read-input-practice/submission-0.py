f238bde23afee6ea5591430902c12cd2b4d6b631
Wojciech Olejnik <Divoolej@users.noreply.github.com>
2026-08-30 22:54:54 +0200

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,11 @@
+def add_two_numbers() -> int:
+    a, b = list(map(int, input().split(",")))
+    return a + b
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
